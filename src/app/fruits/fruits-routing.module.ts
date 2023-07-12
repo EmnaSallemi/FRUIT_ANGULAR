58d8a7dea60d4eb998db9aa6f1854da869a93ab3
EmnaSallemi <emna.sallemi@ensi-uma.tn>
2023-07-12 15:57:00 +0100

    Generate A 'Edit' Component:

--- src/app/fruits/fruits-routing.module.ts
+++ src/app/fruits/fruits-routing.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
 const routes: Routes = [
   {
     path: 'fruits/home',
     component: HomeComponent,
-    ///path: 'fruits/create',
-    //component: CreateComponent,
 
   },
+  {
+    path:'fruits/edit/:id',
+    component: EditComponent
+  },
+  {
+    path: 'fruits/home',
+    component: HomeComponent,
+  }
 ];
 
 @NgModule({
