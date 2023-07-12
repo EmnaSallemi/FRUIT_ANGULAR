1bb398b97c1e2a855cb9962cbf37178eb34f96e5
EmnaSallemi <emna.sallemi@ensi-uma.tn>
2023-07-12 16:28:10 +0100

    Update Operation

--- src/app/fruits/edit/edit.component.ts
+++ src/app/fruits/edit/edit.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fruits } from '../fruits';
+import { FruitsService } from '../fruits.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
-
+export class EditComponent implements OnInit {
+  fruitForm: Fruits = {
+    id: 0,
+    name: '',
+    price: 0,
+    quantity: 0,
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private router:Router,
+    private fruitService: FruitsService
+  ) {}
+ 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((param) => {
+      var id = Number(param.get('id'));
+      this.getById(id);
+    });//read the 'id' value from the route using the 'ActivatedRoute.paramMap.subscribe()'
+  }
+ 
+  getById(id: number) {
+    this.fruitService.getById(id).subscribe((data) => {
+      this.fruitForm = data;
+    });
+  }
+ 
+  update() {
+    this.fruitService.update(this.fruitForm)
+    .subscribe({
+      next:(data) => {
+        this.router.navigate(["/fruits/home"]);
+      },
+      error:(err) => {
+        console.log(err);
+      }
+    })
+  }
 }
